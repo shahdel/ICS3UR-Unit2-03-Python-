efa0609ffa8c5e6c91652623f234247ef9bff814
EC2 Default User <ec2-user@ip-172-31-45-129.ec2.internal>
2021-12-26 06:40:50 +0000

    my pizza.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Shahd Elsaman
+# Created on: December 2021
+# This module contains constants for the pizza program
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
